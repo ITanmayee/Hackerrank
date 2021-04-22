a24c5619b5db714a56df0a4a2b1e57fa38619ffb
ITanmayee <67579192+ITanmayee@users.noreply.github.com>
2021-04-22 15:35:33 +0530

    To find the minimum number of required deletions such that there are no ajacent characters .

---
+++ Alternating_characters.py
@@ -0,0 +1,11 @@
+# You are given a string containing characters A and B only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
+# Your task is to find the minimum number of required deletions.
+# For example, given the string s = AABAAB , remove an A at positions 0 and 3 to make s = ABAB in 2 deletions.
+
+def alternatingCharacters(s):
+    return len([s[i] for i in range(1 , len(s)) if s[i - 1] == s[i]])
+
+# print(alternatingCharacters("AAAA"))
+# print(alternatingCharacters("BBBBB"))
+# print(alternatingCharacters("ABABABAB"))
+# print(alternatingCharacters("AAABBB"))
